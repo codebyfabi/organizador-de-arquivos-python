1a612a5199adba403498321a81d1cd2c19d9cd5c
Fabiana <fabi01121519@gmail.com>
2026-06-23 17:57:50 -0300

    organização script

--- Organizador_documentos.py
+++ Organizador_documentos.py
@@ -1,16 +1,13 @@
-#Organizador de documentos
 import os
 import shutil
 
-# O Python descobre automaticamente a pasta raiz do usuário atual
-# Isso funciona para qualquer pessoa, em qualquer PC!
+
 pasta_base = os.path.expanduser("~")
 
-# Definimos os caminhos de forma dinâmica
 pasta_origem = os.path.join(pasta_base, "Downloads")
 pasta_destino_base = os.path.join(pasta_base, "Documentos", "Organizados")
 
-# Define a extensão e o nome da pasta de destino
+#Define a extensão e o nome da pasta de destino
 configuracao = {
     ".docx": "Documentos_Word",
     ".pdf": "PDFs",
@@ -21,7 +18,6 @@ configuracao = {
 }
 
 def organizar_arquivos():
-    # Verifica se a pasta de origem existe antes de começar
     if not os.path.exists(pasta_origem):
         print(f"Erro: A pasta {pasta_origem} não foi encontrada.")
         return
@@ -32,17 +28,17 @@ def organizar_arquivos():
         for extensao, nome_pasta in configuracao.items():
             if arquivo.lower().endswith(extensao):
                 
-                # Define o caminho completo da pasta de destino
+           
                 destino = os.path.join(pasta_destino_base, nome_pasta)
                 
-                # CRIA A PASTA AUTOMATICAMENTE
+             
                 os.makedirs(destino, exist_ok=True)
                 print(f"DEBUG: Garantindo que a pasta existe em: {destino}") 
-                # Monta os caminhos
+            
                 origem_completa = os.path.join(pasta_origem, arquivo)
                 destino_completo = os.path.join(destino, arquivo)
                 
-                # Move o arquivo
+                
                 shutil.move(origem_completa, destino_completo)
                 print(f"Sucesso: {arquivo} movido para {destino}")
 
